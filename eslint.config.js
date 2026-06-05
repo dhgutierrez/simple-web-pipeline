6e5c366169bde8203a058b8ec94edb1d927e4cbc
Daniel Huanca <daniel.huanca@gmail.com>
2026-06-05 19:39:41 -0300

    Fix eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,17 @@
 import js from "@eslint/js";
 
 export default [
-  js.configs.recommended
+  {
+    ignores: ["dist/**", "node_modules/**"]
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        document: "readonly",
+        MutationObserver: "readonly",
+        fetch: "readonly"
+      }
+    }
+  }
 ];
